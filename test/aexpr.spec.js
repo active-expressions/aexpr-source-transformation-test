302eeb54594077e1f995a4ddd518b3492dfe8e83
Stefan Lehmann <stefan.lehmann@hpi.de>
2016-08-12 11:07:22 +0200

    a project can finally use rewriting aexprs

--- test/aexpr.spec.js
+++ test/aexpr.spec.js
@@ -1,21 +1,88 @@
 'use strict';
 
-describe('Sinon', function() {
+describe('Propagation Logic', function() {
 
-    it('should be supported', () => {
-        var spy = sinon.spy();
+    it('is a transparent wrapper for property accesses', () => {
+        let obj = {
+            prop: 42,
+            func(mul) { return this.prop * mul}
+        };
 
-        spy(42);
+        expect(obj.prop).to.equal(42);
+        expect(obj.func(2)).to.equal(84);
 
-        expect(spy.withArgs(42).calledOnce).to.be.true;
+        obj.prop /= 3;
+
+        expect(obj.prop).to.equal(14);
+        expect(obj.func(2)).to.equal(28);
     });
 
     it('should be supported with proper integration', () => {
-        var spy = sinon.spy();
+        let obj = { prop: 42 },
+            spy = sinon.spy();
 
-        spy(42);
+        aexpr(() => obj.prop).onChange(spy);
 
-        expect(spy).to.be.calledWith(42);
+        expect(spy).not.to.be.called;
+
+        obj.prop = 17;
+
+        expect(spy).to.be.calledOnce;
     });
 
+    it('should recalculate to recognize latest changes', () => {
+        let obj = {
+                prop: 'a',
+                a: 15,
+                b: 32
+            },
+            spy = sinon.spy();
+
+        aexpr(() => obj[obj.prop]).onChange(spy);
+
+        obj.a = 17;
+
+        expect(spy.withArgs(17)).to.be.calledOnce;
+
+        obj.prop = 'b';
+
+        expect(spy.withArgs(32)).to.be.calledOnce;
+
+        obj.a = 42;
+
+        expect(spy.withArgs(42)).not.to.be.called;
+
+        obj.b = 33;
+
+        expect(spy.withArgs(33)).to.be.calledOnce;
+    });
+
+    it('applies the given operator', () => {
+        let obj = {
+                a: 5
+            },
+            spy = sinon.spy();
+
+        aexpr(() => obj.a).onChange(spy);
+
+        obj.a *= 1;
+
+        expect(spy).not.to.be.called;
+    });
+
+    it('retain the this reference semantic', () => {
+        let obj = {
+                a: 5,
+                func() {
+                    return this.a * 3;
+                }
+            },
+            spy = sinon.spy();
+
+        aexpr(() => obj.func()).onChange(spy);
+
+        obj.a = 1;
+
+        expect(spy.withArgs(3)).to.be.calledOnce;
+    });
 });
